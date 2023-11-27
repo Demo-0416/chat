089c4c068d10dd489231de8aaa2bd52ae3394ae0
hnuwangjue <305925092@qq.com>
2023-11-27 11:03:33 +0800

    11周注册、登录接口（完成）

--- chat v2/src/main/java/com/example/chat/UserController.java
+++ chat v2/src/main/java/com/example/chat/UserController.java
@@ -39,21 +39,15 @@ public class UserController {
     return userMapper.findAll();
   }
 
-  @PostMapping
+  @PostMapping("/manageadduser")
   public String addUser(@RequestBody com.example.chat.User user) {
     userMapper.save(user);
     return "成功";
   }
 
-  @PutMapping
-  public String updateUser(@RequestBody com.example.chat.User user) {
-    userMapper.updateById(user);
-    return "成功";
-  }
-
-  @DeleteMapping("/{userId}")
-  public String deleteUser(@PathVariable("userId") Long id) {
-    userMapper.deleteById(id);
+  @DeleteMapping("/{email}")
+  public String deleteUser(@PathVariable("email")String email) {
+    userMapper.deleteById(email);
     return "成功";
   }
 
@@ -82,6 +76,28 @@ public class UserController {
       return registerCode;
     } else {
       return "邮箱已注册";
+    }
+  }
+
+  @GetMapping("/findpassword")
+  public String UserFindPassword(HttpServletRequest request){
+    GetCodeNumber number = new GetCodeNumber();
+    String email = request.getParameter("email");
+    User user = userMapper.findByEmail(email);
+
+    if(user != null) {
+
+      String registerCode = number.GetNumber(email);
+
+      String email1Register = userMapper.findRegisterByEmail(email);
+      if(email1Register == null){
+        userMapper.addRegister(email, registerCode);
+      } else {
+        userMapper.updateRegister(email, registerCode);
+      }
+      return registerCode;
+    } else {
+      return "邮箱未注册";
     }
   }
 
@@ -119,11 +135,11 @@ public class UserController {
   public String userLogin(HttpServletRequest request){
     String email=request.getParameter("email");
     String password=request.getParameter("password");
+    String cookie = request.getParameter("cookie");
     User user=new User();
     user.setUserEmail(email);
     user.setUserPassword(password);
-    User user1=new User();
-    //System.out.println(user1.getName());
+    User user1;
     if(user.getUserEmail() == null){
       return "请输入邮箱号码";
     }else {
@@ -131,18 +147,26 @@ public class UserController {
       if(user1 == null){
         return "用户不存在";
       }else {
-        System.out.println(user1.getUserPassword());
         if(user1.getUserPassword().equals(password)){
-          HttpSession session= request.getSession();
-          session.setAttribute("email",user.getUserEmail());
+          if(userMapper.findLogInUser(cookie) == null){
+            userMapper.addLogIn(email, cookie);
+          } else {
+            userMapper.updateLogInUser(email, cookie);
+          }
+
           return "登录成功";
         } else {
           return "密码错误";
         }
-
-
       }
     }
+  }
+
+  @GetMapping("/deletelogged")
+  public String deleteLogIn(HttpServletRequest request){
+    String cookie = request.getParameter("cookie");
+    userMapper.deleteLoggedByEmail(cookie);
+    return "退出成功";
   }
 
 }
